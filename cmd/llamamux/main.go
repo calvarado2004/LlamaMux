293168962111ac591ed43f885e831fe51433a263
calvarado2004 <carlos-alvarado@outlook.com>
2025-11-23 17:47:42 -0500

    adding some logging.

--- cmd/llamamux/main.go
+++ cmd/llamamux/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/calvarado2004/LlamaMux/internal/api"
 	"github.com/calvarado2004/LlamaMux/internal/config"
 )
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		duration := time.Since(start)
+		log.Printf("%s %s %s (%s)", r.RemoteAddr, r.Method, r.URL.Path, duration)
+	})
+}
 
 func main() {
 	cfg := config.Load()
@@ -15,9 +25,10 @@ func main() {
 	mux := http.NewServeMux()
 	srv.RegisterRoutes(mux)
 
+	handler := loggingMiddleware(mux)
+
 	log.Printf("LlamaMux listening on %s", cfg.ListenAddr)
-	if err := http.ListenAndServe(cfg.ListenAddr, mux); err != nil {
+	if err := http.ListenAndServe(cfg.ListenAddr, handler); err != nil {
 		log.Fatalf("server error: %v", err)
 	}
 }
-
